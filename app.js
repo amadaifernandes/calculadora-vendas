cef633be890038e2dc88a803a4860e9362baeb41
amadaifernandes <124642973+amadaifernandes@users.noreply.github.com>
2026-04-01 18:03:44 -0300

    Refactor subtotal calculation logic
    Removed unnecessary rounding for subtotal calculation.

--- app.js
+++ app.js
@@ -54,11 +54,9 @@ function calcular() {
         if (qty > 0) {
             count++;
             units += qty;
-            const preco=prices[parseInt(input.dataset.idx)]
+            const preco = prices[parseInt(input.dataset.idx)]
             const subtotal = qty * preco;
-            
-//Arredondamentos por item (igual excel)
-            total += Number(subtotal.toFixed(2));
+            total += qty * _prices[parseInt(input.dataset.idx)];
             
             //total += qty * _prices[parseInt(input.dataset.idx)];
         }
